79ddf2af226e729d599f23b1ceeae12397228f95
Student <jsitbon@qcc.cuny.edu>
2019-03-15 09:12:54 -0400

    spa

---
+++ midterm/js/spa.js
@@ -0,0 +1,15 @@
+var app = angular.module('midtermApp', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'app/views/home.html'
+  })
+  .when('/works', {
+    templateUrl: 'app/views/works.html'
+  })
+  .when('/news', {
+    templateUrl: 'app/views/news.html'
+  })
+  .when('/contact', {
+    templateUrl: 'app/views/contact.html'
+  })
+});
